ef2d8abf73f7cc2b2a322afc65bd92ebf83cd3a3
Brett Shearin <brettcshearin@hotmail.com>
2018-06-13 10:04:39 -0500

    customers.js

---
+++ customers.js
@@ -0,0 +1,40 @@
+const customers= {
+    seth:{
+        genre: "Science-Fiction",
+        bookshelf: "",
+        libraryCard: "",
+        firstName: "Seth",
+        lastName: "Dana",
+        address: "2142 Rosemary Lane, Nashville TN 37210",
+        checkout: "",
+        return: ""
+    },
+    shu:{
+        genre: "Science-Fiction",
+        bookshelf: "",
+        libraryCard: "",
+        firstName: "Shuaib",
+        lastName: "Sajid",
+        address: "132 Brian Cir. Antioch,TN 37013",
+        checkout: "",
+        return: ""
+    },
+    jake:{
+        genre: "Fantasy",
+        bookshelf: "",
+        libraryCard: "",
+        firstName: "Jake",
+        lastName: "Neild",
+        address:"100 interstate dr, nashville, TN 66666",
+        checkout: "",
+        return: ""
+    },
+    brett:{
+        genre: "Fantasy",
+        bookshelf: "",
+        libraryCard: "",
+        firstName: "Brett",
+        lastName: "Shearin",
+        address: "954 Hawkins Rd, White Bluff, TN 37187"
+    } 
+}
