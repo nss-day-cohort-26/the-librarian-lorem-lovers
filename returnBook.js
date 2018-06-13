2b4f48f5bd6ee7a8467941a5eb00c0822fb58bbc
Brett Shearin <brettcshearin@hotmail.com>
2018-06-13 13:52:50 -0500

    added return

--- returnBook.js
+++ returnBook.js
@@ -6,13 +6,18 @@ const returnBook = (titleOfBook, authorOfBook, custName) => {
    //make sure book is checked out by customer
    //change boolean to false
    //remove from bookshelf
-    let currentBook = bookArray.filter((value) => {
+    let currentBook = customers[custName].bookshelf.filter((value) => {
         return (titleOfBook === value.title && authorOfBook === value.author)
     })
     console.log(currentBook)
     switch (currentBook[0].checkedOut){
         
         case (true):
+            let dateNow = moment()
+            if (moment().isAfter(currentBook[0].dueDate) ){
+                alert("You owe $5")
+            
+            }
             currentBook[0].checkedOut = false
             customers[custName].bookshelf.pop(currentBook[0])
             alert("Returned your book")
