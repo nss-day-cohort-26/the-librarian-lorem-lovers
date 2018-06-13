82c3d3986b02700a3ed51bb76fbd80d7b8c1c767
Jake Neild <jakeneildfb@yahoo.com>
2018-06-13 13:52:19 -0500

    Interface for return

--- genreSelector.js
+++ genreSelector.js
@@ -3,7 +3,8 @@ var selectedGenre = "empty";
 function genreSelector(e){
     if(e.target.classList == "menu-item"){
         selectedGenre = e.target.id;
-        document.getElementsByClassName("dropbtn")[0].textContent = e.target.id;
+        let genreName = selectedGenre.slice(0, 1).toUpperCase() + selectedGenre.slice(1, selectedGenre.length)
+        document.getElementsByClassName("dropbtn")[0].textContent = genreName;
         console.log(selectedGenre)
     }
 }
